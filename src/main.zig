df183fbf4a6dbb71fbdbe737e2189961d2133708
Nhân <hi@imnhan.com>
2025-03-30 00:47:38 +0700

    use SDL_WaitEvent; also tidy up the code a little

--- src/main.zig
+++ src/main.zig
@@ -31,44 +31,33 @@ fn appInit(appstate: ?*?*anyopaque, argc: c_int, argv: ?[*:null]?[*:0]u8) callco
     _ = argv;
     _ = c.SDL_SetAppMetadata("Example HUMAN READABLE NAME", "1.0", "com.example.CATEGORY-NAME");
 
-    if (!c.SDL_Init(c.SDL_INIT_VIDEO)) {
-        c.SDL_Log("Couldn't initialize SDL: %s", c.SDL_GetError());
-        return c.SDL_APP_FAILURE;
-    }
-
-    if (!c.SDL_CreateWindowAndRenderer("examples/CATEGORY/NAME", 640, 480, 0, &window, &renderer)) {
-        c.SDL_Log("Couldn't create window/renderer: %s", c.SDL_GetError());
-        return c.SDL_APP_FAILURE;
-    }
-
-    if (!c.SDL_SetWindowResizable(window, true)) {
-        c.SDL_Log("Couldn't make window resizable: %s", c.SDL_GetError());
-        return c.SDL_APP_FAILURE;
-    }
-
+    assert(c.SDL_Init(c.SDL_INIT_VIDEO));
+    assert(c.SDL_CreateWindowAndRenderer("examples/CATEGORY/NAME", 640, 480, 0, &window, &renderer));
+    assert(c.SDL_SetWindowResizable(window, true));
     return c.SDL_APP_CONTINUE; // carry on with the program!
 }
 
 // This function runs when a new event (mouse input, keypresses, etc) occurs.
 fn appEvent(appstate: ?*anyopaque, event: ?*c.SDL_Event) callconv(.c) c.SDL_AppResult {
     _ = appstate;
-    if (event.?.type == c.SDL_EVENT_QUIT) {
-        return c.SDL_APP_SUCCESS; // end the program, reporting success to the OS.
+
+    switch (event.?.type) {
+        c.SDL_EVENT_QUIT => return c.SDL_APP_SUCCESS,
+        else => return c.SDL_APP_CONTINUE,
     }
-    return c.SDL_APP_CONTINUE; // carry on with the program!
 }
 
 // This function runs once per frame, and is the heart of the program.
 fn appIterate(appstate: ?*anyopaque) callconv(.c) c.SDL_AppResult {
     _ = appstate;
 
-    assert(c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
-    assert(c.SDL_RenderClear(renderer));
-
     var window_w: c_int = undefined;
     var window_h: c_int = undefined;
     assert(c.SDL_GetWindowSize(window, &window_w, &window_h));
-    std.debug.print(">> window size: {d}x{d}\n", .{ window_w, window_h });
+    std.debug.print(
+        ">> {d}: {d}x{d}\n",
+        .{ std.time.milliTimestamp(), window_w, window_h },
+    );
 
     assert(c.SDL_SetRenderDrawColor(renderer, 64, 64, 255, 255));
     assert(c.SDL_RenderFillRect(renderer, &c.SDL_FRect{
@@ -79,6 +68,7 @@ fn appIterate(appstate: ?*anyopaque) callconv(.c) c.SDL_AppResult {
     }));
     assert(c.SDL_RenderPresent(renderer));
 
+    assert(c.SDL_WaitEvent(null));
     return c.SDL_APP_CONTINUE; // carry on with the program!
 }
 
